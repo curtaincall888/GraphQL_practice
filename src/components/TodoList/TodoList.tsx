40165182106efb1142810cf3bcc658f36c0e08b4
kota-t <kota-t@weseek.co.jp>
2023-06-18 22:59:06 +0900

    add TodoList component

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,29 @@
+import type { FC, FormEventHandler } from 'react'
+import { useEffect, useState } from 'react'
+
+import type { TodosQuery } from '@/generated/request'
+import {
+  useAddTodoMutation,
+  useDeleteTodoMutation,
+  useTodosQuery,
+  useUpdateTodoMutation,
+} from '@/generated/request'
+
+export const TodoList: FC = () => {
+  const [todoTitle, setTodoTitle] = useState('')
+  const [todos, setTodos] = useState<TodosQuery['todos']>([])
+  const { data, loading, error } = useTodosQuery()
+  const [addTodoMutation] = useAddTodoMutation()
+  const [updateTodoMutation] = useUpdateTodoMutation()
+  const [deleteTodoMutation] = useDeleteTodoMutation()
+  useEffect(() => {
+    setTodos(data?.todos ?? [])
+  }, [data?.todos])
+
+  console.log(error)
+  if (loading) return <div>loading...</div>
+  if (error) return <div>error!</div>
+  if (!data?.todos) return null
+
+  return <div className="p-5 border rounded">Todo List</div>
+}
